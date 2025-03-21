86a29a0904f7ae50bbf9f25ff62b929e90765e2e
Alex <Alex@192.168.1.32>
2025-03-20 18:43:02 -0600

    removed inessential files, added to django admin all models

--- subsy_backend/app/core/admin.py
+++ subsy_backend/app/core/admin.py
@@ -5,3 +5,9 @@ from . import models
 # Register your models here.
 admin.site.register(models.User)
 admin.site.register(models.Company)
+admin.site.register(models.LinkedBank)
+admin.site.register(models.BankAccount)
+admin.site.register(models.Transaction)
+admin.site.register(models.Application)
+admin.site.register(models.Subscription)
+admin.site.register(models.Tag)
--- subsy_backend/app/user/views.py
+++ subsy_backend/app/user/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+from core.views import BaseAuthPermissions
 from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
@@ -12,7 +13,7 @@ from user.serializers import (
 
 
 class CreateUserView(generics.CreateAPIView):
-    """Create a new user in the system."""
+    """Create a new user in the db."""
     serializer_class = UserSerializer
     permission_classes = []
 
@@ -23,11 +24,9 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveUpdateAPIView):
+class ManageUserView(BaseAuthPermissions, generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
