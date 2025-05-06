2469a73978acfc2dc812ce3066e3eb00730b6440
Alex <ajaimepozas@gmail.com>
2025-05-05 18:32:09 -0600

    date examples

--- subsy_backend/examples.py
+++ subsy_backend/examples.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timezone
 
 current_utc_date = datetime.now(timezone.utc).date()
+current_date = datetime.now(timezone.utc)
 print(current_utc_date)
+print(current_date)
