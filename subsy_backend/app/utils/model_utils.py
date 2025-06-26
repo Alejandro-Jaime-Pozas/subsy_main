90ed9183dc26e520cddda4d3f98a8587f73abd3a
Alex <ajaimepozas@gmail.com>
2025-06-25 22:43:45 -0600

    adding app and sub fns to create based on new transactions

---
+++ subsy_backend/app/utils/model_utils.py
@@ -0,0 +1,11 @@
+"""
+Utils for model-related operations.
+"""
+
+
+def create_application_if_not_exists(transactions: dict):
+    pass
+
+
+def update_or_create_subscription(transactions: dict):
+    pass 
